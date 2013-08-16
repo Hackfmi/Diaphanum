19a8482a4e200ffb17b317144b6559f051dec4d6
hhursev <hhursev@gmail.com>
2013-08-17 01:15:27 +0300

    initial Django setup commit

---
+++ hackfmi/example_local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hackfmi',
+        'USER': 'miimir',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
