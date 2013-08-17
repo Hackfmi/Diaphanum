a02f2a1ba8f2cbf0cda0a6b8b794c4970bb4b4f2
filareta <filareta_yordanova@abv.bg>
2013-08-17 18:30:11 +0300

    Add url for media files

--- hackfmi/urls.py
+++ hackfmi/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,4 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,}),
 )
