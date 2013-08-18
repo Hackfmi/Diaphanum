7017a6d38dffd3145946a12b8c7c89da89740b9d
hhursev <hhursev@gmail.com>
2013-08-18 23:22:49 +0300

    Fix broken tests, because of the models change

--- reports/tests.py
+++ reports/tests.py
@@ -1,8 +1,9 @@
 from django.test import client, TestCase
 
 from members.models import User
+from datetime import time
 from .models import Report
-from protocols.models import Topic
+from protocols.models import Topic, Institution, Protocol
 
 
 client = client.Client()
@@ -17,24 +18,41 @@ class ReportTest(TestCase):
         self.kril.set_password('kril')
         self.kril.save()
 
+        self.institution = Institution.objects.create(
+            name='some institution name')
+
+        self.protocol = Protocol.objects.create(
+            institution=self.institution,
+            number='2341',
+            scheduled_time=time(9, 0, 0),
+            start_time=time(10, 0, 0),
+            quorum=32,
+            majority=5,
+            current_majority=4,
+            information="best protocol ever",
+            )
+
         self.topic1 = Topic.objects.create(
             name='1',
             description='first',
             voted_for=13,
             voted_against=4,
-            voted_abstain=5)
+            voted_abstain=5,
+            protocol=self.protocol)
         self.topic2 = Topic.objects.create(
             name='2',
             description='second',
             voted_for=13,
             voted_against=4,
-            voted_abstain=5)
+            voted_abstain=5,
+            protocol=self.protocol)
         self.topic3 = Topic.objects.create(
             name='3',
             description='third',
             voted_for=13,
             voted_against=4,
-            voted_abstain=5)
+            voted_abstain=5,
+            protocol=self.protocol)
 
     def test_add_report(self):
         client.login(username='Kril', password='kril')
