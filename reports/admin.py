c25c9de19369467116714eadff86984f716d9b90
Peter Sabev <peter.p.sabev@gmail.com>
2013-08-18 14:44:32 +0300

    Change fields order

--- reports/admin.py
+++ reports/admin.py
@@ -3,11 +3,8 @@ from django.contrib import admin
 from .models import Report
 
 class ReportAdmin(admin.ModelAdmin):
-    #Определя кои полета да се показват в заявката за извлизачне на анкети
-    list_display = ('addressed_to', 'reported_from', 'content', 'created_at', 'signed_from', 'get_copies')
-    #Добавя поле за филтриране
+    list_display = ('addressed_to', 'reported_from', 'content', 'signed_from', 'get_copies', 'created_at')
     list_filter = ['created_at']
-    #Добавя поле за търсене по полето от БД въпрос
     search_fields = ['addressed_to', 'reported_from', 'content', 'signed_from']
 
 admin.site.register(Report, ReportAdmin)
