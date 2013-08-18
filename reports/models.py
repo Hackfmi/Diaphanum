d5c296197c7f5b422f44e58f8e58ead5fdc5c2ad
hhursev <hhursev@gmail.com>
2013-08-18 16:40:09 +0300

    Add new initial migration for reports

--- reports/models.py
+++ reports/models.py
@@ -1,14 +1,12 @@
 from datetime import datetime
-
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 
 class Report(models.Model):
     addressed_to = models.TextField()
     reported_from = models.ForeignKey('members.User')
     content = models.TextField()
-    created_at = models.DateField(_("Date"), default=datetime.now())
+    created_at = models.DateField(default=datetime.now)
     copies = models.ManyToManyField('protocols.Topic')
     signed_from = models.CharField(max_length=64)
 
