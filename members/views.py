a0fa76a7aeb3dba3b358abeab95fc03a90a0e8b6
filareta <filareta_yordanova@abv.bg>
2013-08-18 21:23:44 +0300

    Add view for searching users and return json format

--- members/views.py
+++ members/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import User
 
 
 def homepage(request):
     return render(request, "index.html", {})
+
+
+def search(request, name):
+    members = User.objects.filter(first_name__icontains=name) or \
+        User.objects.filter(last_name__icontains=name) or \
+        User.objects.filter(username__icontains=name)
+
+    json_data = [dict(
+        id=member.id,
+        full_name=' '.join([member.first_name, member.last_name]))
+                for member in members]
+
+    return HttpResponse(json_data, mimetype='application/json')
