218aab63d87d6537c6705f6228a5f49e61d27f8f
Victoria Hristova <viki.hristova@gmail.com>
2013-08-18 11:59:30 +0300

    Make migration to fix Topic's attachments

--- protocols/models.py
+++ protocols/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Topic(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    attachment = models.ManyToManyField(Attachment)
+    attachment = models.ManyToManyField('attachments.Attachment')
 
     def __unicode__(self):
         return self.name
