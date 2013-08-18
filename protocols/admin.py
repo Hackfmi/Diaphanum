0988a92bb91f1d203064bac7a76d7b3a86f1bfb9
Peter Sabev <peter.p.sabev@gmail.com>
2013-08-18 15:58:15 +0300

    Add protocols app admin customization and search feature

--- protocols/admin.py
+++ protocols/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Protocol, Topic
 
+class ProtocolAdmin(admin.ModelAdmin):
+
+    list_display = ['number', 'date', 'scheduled_time', 'start_time', 'get_topics', 'information', 'voted_for',
+                    'voted_against', 'voted_abstain']
+
+    list_display_links = ['number']
+
+    list_filter = ['topics', 'date']
+
+    search_fields =['number', 'information', 'topics__name', 'topics__description']
 
 admin.site.register(Topic)
-admin.site.register(Protocol)
+admin.site.register(Protocol, ProtocolAdmin)
