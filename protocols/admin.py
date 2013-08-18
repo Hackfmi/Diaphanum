d653dcec9327da916816cc2ddf008897af91e35f
Peter Sabev <peter.p.sabev@gmail.com>
2013-08-18 17:36:00 +0300

    Comment protocols/admin.py code, because of model changes

--- protocols/admin.py
+++ protocols/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from .models import Protocol, Topic
 
 
-class ProtocolAdmin(admin.ModelAdmin):
-    list_display = ['number', 'date', 'scheduled_time', 'start_time', 'get_topics', 'information', 'voted_for',
-                    'voted_against', 'voted_abstain']
-
-    list_display_links = ['number']
-
-    list_filter = ['topics', 'date']
-
-    search_fields =['number', 'information', 'topics__name', 'topics__description']
+# class ProtocolAdmin(admin.ModelAdmin):
+#     list_display = ['number', 'date', 'scheduled_time', 'start_time', 'get_topics', 'information', 'voted_for',
+#                     'voted_against', 'voted_abstain']
+#
+#     list_display_links = ['number']
+#
+#     list_filter = ['topics', 'date']
+#
+#     search_fields =['number', 'information', 'topics__name', 'topics__description']
 
 
 admin.site.register(Topic)
