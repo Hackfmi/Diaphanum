3774748323e7657cb25fe9a6e45a5ea9cf379dff
Ivaylo Bachvarov <bachvarof@gmail.com>
2013-08-20 00:38:10 +0300

    Migrate protocols form to html and css name convention

--- hackfmi/static/js/protocolsForm.js
+++ hackfmi/static/js/protocolsForm.js
@@ -3,37 +3,37 @@ $(document).ready(function(){
 		 .on("click", ".add-excused", function(){
 	      var newTeamMemberHtml = $("#new-excused").html();
 	      // use underscore if any placeholders
-	      $(newTeamMemberHtml).insertBefore("#excused-control");
-	      // hack for now
-	      // TypeAheader.feed($("input.autocomplete").not(".tt-query"),
-	      //                   typeAheadConfig,
-	      //                   typeAheadSelectCallback);
-	    })
-	    .on("click", ".remove-excused", function(){
-	      $(this).parent().remove();
-	    })
-	 	.on("click", ".add-absent", function(){
-	      var newTeamMemberHtml = $("#new-absent").html();
-	      // use underscore if any placeholders
-	      $(newTeamMemberHtml).insertBefore("#absent-control");
-	      // hack for now
-	      // TypeAheader.feed($("input.autocomplete").not(".tt-query"),
-	      //                   typeAheadConfig,
-	      //                   typeAheadSelectCallback);
+		  $(newTeamMemberHtml).insertBefore("#excused-control");
+		  // hack for now
+		  // TypeAheader.feed($("input.autocomplete").not(".tt-query"),
+		  //                   typeAheadConfig,
+		  //                   typeAheadSelectCallback);
 		})
-	    .on("click", ".remove-absent", function(){
-	      $(this).parent().remove();
-	    })
-	 	.on("click", ".add-attendents", function(){
-	      var newTeamMemberHtml = $("#new-attendents").html();
-	      // use underscore if any placeholders
-	      $(newTeamMemberHtml).insertBefore("#attendents-control");
+		.on("click", ".remove-excused", function(){
+		  $(this).parent().remove();
+		})
+			.on("click", ".add-absent", function(){
+		  var newTeamMemberHtml = $("#new-absent").html();
+		  // use underscore if any placeholders
+		  $(newTeamMemberHtml).insertBefore("#absent-control");
+		  // hack for now
+		  // TypeAheader.feed($("input.autocomplete").not(".tt-query"),
+		  //                   typeAheadConfig,git
+		  //                   typeAheadSelectCallback);
+		})
+		.on("click", ".remove-absent", function(){
+		  $(this).parent().remove();
+		})
+			.on("click", ".add-attendents", function(){
+		  var newTeamMemberHtml = $("#new-attendents").html();
+		  // use underscore if any placeholders
+		  $(newTeamMemberHtml).insertBefore("#attendents-control");
 		// hack for now
 		// TypeAheader.feed($("input.autocomplete").not(".tt-query"),
 		//                   typeAheadConfig,
 		//                   typeAheadSelectCallback);
-	    })
-	    .on("click", ".remove-attendents", function(){
-	      $(this).parent().remove();
-	    })
+		})
+		.on("click", ".remove-attendents", function(){
+		  $(this).parent().remove();
+		})
 });
