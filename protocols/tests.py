3e26e709455d68de8960d33b7214dc1c307af6a5
hhursev <hhursev@gmail.com>
2013-08-19 17:04:03 +0300

    Add tests who can edit a form and who cannot

--- protocols/tests.py
+++ protocols/tests.py
@@ -4,6 +4,7 @@ from django.test import client, TestCase
 
 from members.models import User
 from .models import Protocol, Topic, Institution
+from django.contrib.auth.models import Permission
 
 
 client = client.Client()
