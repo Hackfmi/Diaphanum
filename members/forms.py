fd0cf7c8d082ed9a3cf742be1a45fbbe3af98e8b
Peter Sabev <peter.p.sabev@gmail.com>
2013-08-19 20:46:26 +0300

    Delete forms.py

--- members/forms.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django import forms
-
-from .models import User
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
