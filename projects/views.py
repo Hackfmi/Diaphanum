ae964fae2b2df5e8d36db152efa3c3ab7a8cf094
filareta <filareta_yordanova@abv.bg>
2013-08-19 20:45:20 +0300

    Add form for editing project status

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
+from django.contrib.auth.decorators import user_passes_test
 
 
 from .models import Project
-from .forms import ProjectForm
+from .forms import ProjectForm, RestrictedProjectForm
 
 
 def can_edit_projects(user):
@@ -29,3 +30,16 @@ def edit_project(request, project_id=None):
         return render(request, 'projects/edit.html', locals())
     else:
         raise Http404
+
+
+# @permissiion_required('edit_status')
+@user_passes_test(can_edit_projects)
+def edit_status(request, project_id=None):
+    project = get_object_or_404(Project, id=project_id)
+    data = request.POST if request.POST else None
+    form = RestrictedProjectForm(data=data, instance=project)
+    import ipdb; ipdb.set_trace()
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'projects/edit_status.html', locals())
