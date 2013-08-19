87c6b9f4b266cc790b06d83a3dd02b96aa7e68f5
hhursev <hhursev@gmail.com>
2013-08-19 20:22:58 +0300

    Add restriction of users who can send reports

--- reports/views.py
+++ reports/views.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
 from .forms import ReportForm
 
 
+def can_add_reports(user):
+    return user.is_authenticated() and user.has_perm('reports.add_report')
+
+
+@user_passes_test(can_add_reports)
 def add_report(request):
     data = request.POST if request else None
     form = ReportForm(data)
