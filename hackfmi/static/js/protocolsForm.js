553c39deaaa7598a88b8f505f16c68b802edc45f
Ivaylo Bachvarov <bachvarof@gmail.com>
2013-08-20 00:14:11 +0300

    Implement option for adding more then one operson where is needed

--- hackfmi/static/js/protocolsForm.js
+++ hackfmi/static/js/protocolsForm.js
@@ -1,28 +1,39 @@
-$(document).ready(function() {
-	$(".protocolForm").validate({
-		rules : {
-			number : {
-				required : true,
-				number: true
-			},
-			institution : {
-				required : true
-			},
-			conducted_at : {
-				date: true,
-				required : true
-			},
-			scheduled_time : {
-				required : true
-			},
-			quorum : {
-				required : true,
-				digits: true
-			},
-			start_time : {
-				required : true
-			},
-			reportContent : window.Diaphanum.appConfig.textAreaValidationReq
-		}
-	});
+$(document).ready(function(){
+	 $(".protocol-form")
+		 .on("click", ".add-excused", function(){
+	      var newTeamMemberHtml = $("#new-excused").html();
+	      // use underscore if any placeholders
+	      $(newTeamMemberHtml).insertBefore("#excused-control");
+	      // hack for now
+	      // TypeAheader.feed($("input.autocomplete").not(".tt-query"),
+	      //                   typeAheadConfig,
+	      //                   typeAheadSelectCallback);
+	    })
+	    .on("click", ".remove-excused", function(){
+	      $(this).parent().remove();
+	    })
+	 	.on("click", ".add-absent", function(){
+	      var newTeamMemberHtml = $("#new-absent").html();
+	      // use underscore if any placeholders
+	      $(newTeamMemberHtml).insertBefore("#absent-control");
+	      // hack for now
+	      // TypeAheader.feed($("input.autocomplete").not(".tt-query"),
+	      //                   typeAheadConfig,
+	      //                   typeAheadSelectCallback);
+		})
+	    .on("click", ".remove-absent", function(){
+	      $(this).parent().remove();
+	    })
+	 	.on("click", ".add-attendents", function(){
+	      var newTeamMemberHtml = $("#new-attendents").html();
+	      // use underscore if any placeholders
+	      $(newTeamMemberHtml).insertBefore("#attendents-control");
+		// hack for now
+		// TypeAheader.feed($("input.autocomplete").not(".tt-query"),
+		//                   typeAheadConfig,
+		//                   typeAheadSelectCallback);
+	    })
+	    .on("click", ".remove-attendents", function(){
+	      $(this).parent().remove();
+	    })
 });
