469b7e8a83308b4ea6ad84d49d7a8aa42274a381
hhursev <hhursev@gmail.com>
2013-08-19 17:07:37 +0300

    Add restrictioins for who can edit the project and who cannot

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, Http404
+
 
 from .models import Project
 from .forms import ProjectForm
+
+
+def can_edit_projects(user):
+    return user.is_authenticated() and user.has_perm('projects.change_project')
 
 
 @login_required
@@ -14,3 +20,12 @@ def add_project(request):
         form.save()
 
     return render(request, 'projects/add.html', locals())
+
+
+@login_required
+def edit_project(request, project_id=None):
+    project = get_object_or_404(Project, id=project_id)
+    if can_edit_projects(request.user) or request.user == project.user:
+        return render(request, 'projects/edit.html', locals())
+    else:
+        raise Http404
