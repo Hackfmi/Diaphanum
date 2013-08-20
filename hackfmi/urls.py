4a8b5070847de9faa2882164c6a55ae9e8c9be34
hhursev <hhursev@gmail.com>
2013-08-20 14:19:14 +0300

    Initial commit from me in my branch

--- hackfmi/urls.py
+++ hackfmi/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    # JUST FOR THE SAKE OF IT
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'members.views.login', name='login'),
