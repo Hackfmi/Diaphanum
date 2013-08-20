c1429d70319b6f39412f1f34297f706b20d78b80
hhursev <hhursev@gmail.com>
2013-08-20 17:42:15 +0300

    Add url leading to student council archive

--- members/urls.py
+++ members/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('members.views',
     url(r'^login/$', 'login', name='login'),
     url(r'^search/(?P<name>.*)/$', 'search', name='search'),
+    url(r'^archive/$', 'archive_student_council', name='archive_student_council'),
 )
