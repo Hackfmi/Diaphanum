f048d62b7831f0364093025c6bf9e3458d1a7b11
filareta <filareta_yordanova@abv.bg>
2013-08-20 20:58:23 +0300

    Add url corresponding to the added view

--- projects/urls.py
+++ projects/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('projects.views',
     url(r'^add/$', 'add_project', name='add_project'),
-    url(r'^edit/(?P<project_id>.*)/$', 'edit_project', name='edit_project'),
-    url(r'^status/(?P<project_id>.*)/$', 'edit_status', name='edit_status'),
-    url(r'^archive/$', 'projects_archive', name='projects_archive'),
+    url(r'^edit/(?P<project_id>\d+)/$', 'edit_project', name='edit_project'),
+    url(r'^status/(?P<project_id>\d+)/$', 'edit_status', name='edit_status'),
+    url(r'^archive/', 'projects_archive', name='projects_archive'),
+    url(r'^archive/review/(?P<project_id>\d+)/$', 'show_project', name='show-project'),
 )
