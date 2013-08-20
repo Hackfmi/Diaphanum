88734c5aaf3bdddd1e41beff3bdb70b27590490c
hhursev <hhursev@gmail.com>
2013-08-20 17:41:29 +0300

    Add url leading to the archive page

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('projects.views',
     url(r'^add/$', 'add_project', name='add_project'),
     url(r'^edit/(?P<project_id>.*)/$', 'edit_project', name='edit_project'),
     url(r'^status/(?P<project_id>.*)/$', 'edit_status', name='edit_status'),
+    url(r'^archive/$', 'projects_archive', name='projects_archive'),
 )
