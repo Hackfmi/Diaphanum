e14c1c5ab4c16d6873156526fa0b911a2031850f
hhursev <hhursev@gmail.com>
2013-08-20 12:41:26 +0300

    Able to edit projects when status is unrevised/returned

--- projects/views.py
+++ projects/views.py
@@ -23,22 +23,24 @@ def add_project(request):
     return render(request, 'projects/add.html', locals())
 
 
-@login_required
 def edit_project(request, project_id=None):
     project = get_object_or_404(Project, id=project_id)
-    if can_edit_projects(request.user) or request.user == project.user:
+    if request.user == project.user and (project.status == 'unrevised'
+                                         or project.status == 'returned'):
+        data = request.POST if request.POST else None
+        form = ProjectForm(data=data, user=request.user, instance=project)
         return render(request, 'projects/edit.html', locals())
     else:
         raise Http404
 
 
-@user_passes_test(can_edit_projects)
 def edit_status(request, project_id=None):
-    project = get_object_or_404(Project, id=project_id)
-    data = request.POST if request.POST else None
-    form = RestrictedProjectForm(data=data, instance=project)
-
-    if form.is_valid():
-        form.save()
-
-    return render(request, 'projects/edit_status.html', locals())
+    if can_edit_projects(request.user):
+        project = get_object_or_404(Project, id=project_id)
+        data = request.POST if request.POST else None
+        form = RestrictedProjectForm(data=data, instance=project)
+        if form.is_valid():
+            form.save()
+        return render(request, 'projects/edit_status.html', locals())
+    else:
+        raise Http404
