9fc88c3897e9c34551f41b4ea901b0ad3a48f11a
Radoslav Georgiev <g2h.horde@gmail.com>
2013-08-20 16:42:43 +0300

    Making attendents field and added fields to work with typeahead

--- hackfmi/static/js/protocolsForm.js
+++ hackfmi/static/js/protocolsForm.js
@@ -23,6 +23,13 @@ $(document).ready(function(){
 	 	  .val(data.id);
 	 };
 
+	 var attendentsCallback = function(data) {
+	 	$(this)
+	 	  .closest(".attendents-member-field")
+	 	  .find("input.attendents-id-container")
+	 	  .val(data.id);
+	 };
+
 	 $(".protocol-form")
 		 .on("click", ".add-excused", function(){
 	      var newTeamMemberHtml = $("#new-excused").html();
@@ -54,6 +61,11 @@ $(document).ready(function(){
 		  var newTeamMemberHtml = $("#new-attendents").html();
 		  // use underscore if any placeholders
 		  $(newTeamMemberHtml).insertBefore("#attendents-error");
+
+		  TypeAheader.feed(
+		  		$("input.autocomplete.attendents").not(".tt-query"), 
+		  		typeAheadConfig, 
+		  		attendentsCallback );
 		})
 		.on("click", ".remove-attendents", function(){
 		  $(this).parent().remove();
@@ -67,4 +79,5 @@ $(document).ready(function(){
 
 	 	TypeAheader.feed($("input.autocomplete.excused"), typeAheadConfig, excusedCallback );
 	 	TypeAheader.feed($("input.autocomplete.absent"), typeAheadConfig, absentCallback );
+	 	TypeAheader.feed($("input.autocomplete.attendents"), typeAheadConfig, attendentsCallback );
 });
