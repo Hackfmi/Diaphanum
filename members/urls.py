cdefb1fdb304939b35f8c881662fa220a57573dc
filareta <filareta_yordanova@abv.bg>
2013-08-21 20:26:39 +0300

    Add url for user's profile

--- members/urls.py
+++ members/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('members.views',
     url(r'^logout/$', 'logout', name='logout'),
     url(r'^search/(?P<name>.*)/$', 'search', name='search'),
     url(r'^archive/$', 'archive_student_council', name='archive_student_council'),
+    url(r'^profile/$', 'user_projects', name='user-projects'),
 )
