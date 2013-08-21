c85acf5d171821946a230acf50702ac13461b9c4
hhursev <hhursev@gmail.com>
2013-08-22 02:44:49 +0300

    Refactoring

--- positions/admin.py
+++ positions/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Position
 
+
 class PositionAdminIndex(admin.ModelAdmin):
     list_display = ['title', 'date']
-
     list_filter = ['date']
-
     search_fields = ['title', 'content']
+
 admin.site.register(Position, PositionAdminIndex)
