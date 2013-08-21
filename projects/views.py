1649378c8d15236d6e86e570c9251717c2a03206
Radoslav Georgiev <g2h.horde@gmail.com>
2013-08-21 21:45:35 +0300

    Quick fix - missing return in the views file

--- projects/views.py
+++ projects/views.py
@@ -31,7 +31,7 @@ def edit_project(request, project_id=None):
         form = ProjectForm(data=data, user=request.user, instance=project)
         if form.is_valid():
             form.save()
-            HttpResponseRedirect(reverse('members:user-projects'))
+            return HttpResponseRedirect(reverse('members:user-projects'))
         return render(request, 'projects/edit.html', locals())
     else:
         return HttpResponseRedirect(reverse('members:user-projects'))
