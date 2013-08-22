efa9763cb4764dd56d204594f36bea042ceb094d
hhursev <hhursev@gmail.com>
2013-08-22 03:43:47 +0300

    Refactoring

--- protocols/urls.py
+++ protocols/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('protocols.views',
     url(r'^archive/review/(?P<protocol_id>.+)/$', 'show_protocol', name='show-protocol'),
     url(r'^archive/(?P<page>.*)/$', 'listing', name='listing'),
-    url(r'^add/$', 'add', name='add_protocol'),
+    url(r'^add/$', 'add', name='add-protocol'),
     url(r'^search/(?P<name>.*)/$', 'search', name='search'),
     url(r'^page/(?P<page>(\d)*)/$', 'listing', name='listing'),
 )
--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('reports.views',
-    url(r'^add/$', 'add_report', name='add_report'),
+    url(r'^add/$', 'add_report', name='add-report'),
     url(r'^archive/(?P<page>.*)/$', 'listing', name='listing'),
 )
