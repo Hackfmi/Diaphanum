edbbdb56b0a2781bc0ddc26e44742bcbe044e636
hhursev <hhursev@gmail.com>
2013-08-22 15:12:26 +0300

    Make copy show in admin from what protocol, what topics it consists

--- reports/admin.py
+++ reports/admin.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from django.contrib import admin
-from .models import Report
+from .models import Report, Copy
 
 
 class ReportAdmin(admin.ModelAdmin):
@@ -8,4 +8,10 @@ class ReportAdmin(admin.ModelAdmin):
     list_filter = ['created_at']
     search_fields = ['addressed_to', 'reported_from__username', 'content', 'signed_from']
 
+
+class CopyAdmin(admin.ModelAdmin):
+    list_display = ('copy_info', )
+
+
 admin.site.register(Report, ReportAdmin)
+admin.site.register(Copy, CopyAdmin)
