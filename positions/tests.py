3b585f632a2726eec270695e8b79a297d20a00fe
hhursev <hhursev@gmail.com>
2013-08-22 18:17:12 +0300

    Refactoring, pep8 and objects.count()

--- positions/tests.py
+++ positions/tests.py
@@ -1,13 +1,12 @@
-#Tests for positions
-
-
 from django.contrib.auth.models import Permission
 from django.test import TestCase, client
 
 from members.models import User
 from .models import Position
 
+
 client_obj_inst = client.Client()
+
 
 class PositionTest(TestCase):
     def setUp(self):
@@ -25,32 +24,32 @@ class PositionTest(TestCase):
 
     def test_add_new_position_with_right_permissions(self):
         client_obj_inst.login(username='admin', password='admin')
-        before_add = Position.objects.all().count()
+        before_add = Position.objects.count()
         response = client_obj_inst.post('/positions/add/', {
             'title': 'Position title',
             'content': 'texttttttttt'})
-        after_add = Position.objects.all().count()
+        after_add = Position.objects.count()
         self.assertEqual(response.status_code, 302)
         self.assertEqual(before_add+1, after_add)
 
 
     def test_add_new_position_with_right_permissions_and_form_errors(self):
         client_obj_inst.login(username='admin', password='admin')
-        before_add = Position.objects.all().count()
+        before_add = Position.objects.count()
         response = client_obj_inst.post('/positions/add/', {
             'title': '',
             'content': ''})
-        after_add = Position.objects.all().count()
+        after_add = Position.objects.count()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(before_add, after_add)
 
 
     def test_add_new_position_with_no_permissions(self):
         client_obj_inst.login(username='not_master', password='not_master')
-        before_add = Position.objects.all().count()
+        before_add = Position.objects.count()
         response = client_obj_inst.post('/positions/add/', {
             'title': 'Position title',
             'content': 'texttttttttt'})
-        after_add = Position.objects.all().count()
+        after_add = Position.objects.count()
         self.assertEqual(response.status_code, 302)
         self.assertEqual(before_add, after_add)
