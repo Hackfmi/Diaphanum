310770bbbf6961a9453ec17b6f9a2fd185375093
hhursev <hhursev@gmail.com>
2013-08-26 16:51:22 +0300

    Url and namespace function for Year/Month archive

--- projects/urls.py
+++ projects/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('projects.views',
     url(r'^edit_status/(?P<project_id>\d+)/$', 'edit_status', name='edit-status'),
     url(r'^archive/$', 'projects_archive', name='projects-archive'),
     url(r'^archive/review/(?P<project_id>\d+)/$', 'show_project', name='show-project'),
+    url(r'^archive/(?P<year>\d{4})/(?P<month>\d{,2})/$', 'projects_year_month', name='projects-year-month'),
 )
