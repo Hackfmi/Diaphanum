45ee71905d12b46ce89cffebba27405339d1a00e
hhursev <hhursev@gmail.com>
2013-08-28 15:27:01 +0300

    Url to /projects/archive/review/versions/id

--- projects/urls.py
+++ projects/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('projects.views',
     url(r'^edit_status/(?P<project_id>\d+)/$', 'edit_status', name='edit-status'),
     url(r'^archive/$', 'projects_archive', name='projects-archive'),
     url(r'^archive/review/(?P<project_id>\d+)/$', 'show_project', name='show-project'),
+    url(r'^archive/review/versions/(?P<project_id>\d+)/$', 'show_project_versions', name='show-project-versions'),
     url(r'^archive/(?P<year>\d{4})/(?P<month>\d{,2})/$', 'projects_year_month', name='projects-year-month'),
 )
