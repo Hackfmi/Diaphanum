2306da420e5db2cc15b99104dc51780f986f6db2
Victoria Hristova <viki.hristova@gmail.com>
2013-09-13 14:54:57 +0300

    Add search by institution

--- protocols/urls.py
+++ protocols/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('protocols.views',
     url(r'^archive/review/(?P<protocol_id>.+)/$', 'show_protocol', name='show-protocol'),
     url(r'^archive/(?P<page>.*)/$', 'listing', name='listing'),
     url(r'^add/$', 'add', name='add-protocol'),
+    url(r'^institution/(?P<searched_institution>.*)/$', 'protocols_by_institution', name='protocols-by-institution'),
     url(r'^search/(?P<name>.*)/$', 'search', name='search'),
     url(r'^page/(?P<page>(\d)*)/$', 'listing', name='listing'),
 )
