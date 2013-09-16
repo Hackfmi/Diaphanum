9b43567800b478ebcda7fd7da5cf1571ecbbdd53
Victoria Hristova <viki.hristova@gmail.com>
2013-09-16 21:44:11 +0300

    Add search by date

--- protocols/urls.py
+++ protocols/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('protocols.views',
     url(r'^archive/(?P<page>.*)/$', 'listing', name='listing'),
     url(r'^add/$', 'add', name='add-protocol'),
     url(r'^institution/(?P<searched_institution>.*)/$', 'protocols_by_institution', name='protocols-by-institution'),
+    url(r'^search/(?P<start_date>.*)/(?P<end_date>.*)/$', 'protocols_by_date_range', name='protocols-by-date-range'),
     url(r'^search/(?P<name>.*)/$', 'search', name='search'),
     url(r'^page/(?P<page>(\d)*)/$', 'listing', name='listing'),
 )
