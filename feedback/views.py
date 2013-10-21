87684b2a6769140bad35e56bb80357a692a87353
Victoria Hristova <viki.hristova@gmail.com>
2013-10-21 21:25:07 +0300

    Send mail with feedback

--- feedback/views.py
+++ feedback/views.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
+from django.core.mail import send_mail
+
 from .forms import FeedbackForm
 
 
-@permission_required('feedback.add_position')
 def add(request):
     data = request.POST if request.POST else None
-    feedback_form = FeedbackForm(data)
-    if feedback_form.is_valid():
-        feedback_form.save()
+    form = FeedbackForm(data)
+    if form.is_valid():
+        form.save()
+        feedback = form.instance
+        send_mail(u"Обратна връзка",
+                u"Подател: {} {} информация: {}".format(feedback.name, feedback.email, feedback.text),
+                "ss@uni-sofia.bg",
+                "ss@uni-sofia.bg")
         return redirect('members:user-projects')
-    return render(request, 'feedback/add_form.html', {'feedback_form': feedback_form})
+    return render(request, 'feedback/add_form.html', {'feedback_form': form})
