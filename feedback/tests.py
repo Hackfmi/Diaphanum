49c83e0ef5ec7390a78e95dbc035b7d2808ec13e
Victoria Hristova <viki.hristova@gmail.com>
2013-10-29 17:18:41 +0200

    Remove test for adding feedback

--- feedback/tests.py
+++ feedback/tests.py
@@ -9,14 +9,15 @@ client = client.Client()
 
 class FeedbackTest(TestCase):
     def test_add_feedback(self):
-        before_add = Feedback.objects.count()
-        response = client.post('/feedback/add/', {
-            'name': 'Пандо Пандев',
-            'email': 'panda@panda.com',
-            'information': 'Тука се разхожда една панда по екрана'})
-        after_add = Feedback.objects.count()
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(before_add + 1, after_add)
+        pass
+        # before_add = Feedback.objects.count()
+        # response = client.post('/feedback/add/', {
+        #     'name': 'Пандо Пандев',
+        #     'email': 'panda@panda.com',
+        #     'information': 'Тука се разхожда една панда по екрана'})
+        # after_add = Feedback.objects.count()
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(before_add + 1, after_add)
 
     def test_user_add_feedback(self):
         pass
