ec542b93219af5f46b1288ee8e61d2eac613009d
Ivaylo-Bachvarov <bachvarof@gmail.com>
2013-10-30 21:23:55 +0200

    Fix errors and values

--- feedback/views.py
+++ feedback/views.py
@@ -8,20 +8,22 @@ from .forms import FeedbackForm
 
 def add(request):
     data = request.POST if request.POST else None
-    form.errors['captcha'] = "Опитайте отново"
     if request.POST:
         form = FeedbackForm(data)
         if form.is_valid():
-                form.save()
-                feedback = form.instance
-                name = feedback.name
-                email = feedback.email
-                information = feedback.information
-                send_mail(u"Обратна връзка",
-                        u"Подател: {} {} информация: {}".format(name, email, information),
-                        "diaphanum.fmi@gmail.com",
-                        ["diaphanum.fmi@gmail.com"])
-                return redirect('members:user-projects')
+            form.save()
+            feedback = form.instance
+            name = feedback.name
+            email = feedback.email
+            information = feedback.information
+            send_mail(u"Обратна връзка",
+                    u"Подател: {} {} информация: {}".format(name, email, information),
+                    "diaphanum.fmi@gmail.com",
+                    ["diaphanum.fmi@gmail.com"])
+            return redirect('members:user-projects')
+        else:
+            form.errors['captcha'] = "Грешен отговор"
+
     else:
         form = FeedbackForm()
 
