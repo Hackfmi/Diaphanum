d445d0145e4c4a30da46c8ad79e23f5f8e8890f7
Victoria Hristova <viki.hristova@gmail.com>
2013-11-11 18:06:58 +0200

    Define clean method of TopicForm

--- protocols/forms.py
+++ protocols/forms.py
@@ -17,15 +17,36 @@ class TopicForm(forms.ModelForm):
         self.save_m2m()
         return instance
 
+    def clean(self):
+        cleaned_data = super(TopicForm, self).clean()
+        files = self.files.values()
+        if self.instance.pk:
+            already_attached = self.instance.files.all()
+        else:
+            already_attached = []
+        if len(files) > 0:
+            cleaned_data['files'] = [Attachment.objects.create(file_name=file) for file in files]
+            for file in files:
+                if file._size > FILE_UPLOAD_MAX_MEMORY_SIZE:
+                    raise forms.ValidationError("This file is bigger than 20MB")
+        elif 'files' in self._errors:
+            del self._errors['files']
+        if len(files) + len(already_attached) > 5:
+            raise forms.ValidationError("You are trying to upload more than 5 files")
+        cleaned_data['files'] = list(cleaned_data['files']) + list(already_attached)
+        return cleaned_data
+
     class Meta:
         model = Topic
-        exclude = ('protocol', 'attachment')
+        exclude = ('protocol')
         fields = (
             "name",
             "voted_for",
             "voted_against",
             "voted_abstain",
-            "statement", )
+            "statement",
+            "attachment",
+        )
 
 
 TopicFormSet = inlineformset_factory(Protocol, Topic, extra=2)
