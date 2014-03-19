72e9ef4932a433c0c58d21b066f5aadc0a51b317
Ivaylo Bachvarov <bachvarof@gmail.com>
2014-03-19 18:07:05 +0200

    Add urls to conect the reports

--- reports/urls.py
+++ reports/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('reports.views',
     url(r'^add/$', 'add_report', name='add-report'),
     url(r'^archive/(?P<page>.*)/$', 'listing', name='listing'),
-    url(r'^show/(?P<id>.*)/$', 'show', name='show-report'),
+    url(r'^show/(?P<id>.*)/$', 'show', name='show'),
 
 )
