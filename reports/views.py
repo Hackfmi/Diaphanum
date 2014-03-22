df32343a60aaf39802953fdfb0270c9e0f5fa477
viki.hristova@gmail.com <viki.hristova@gmail.com>
2014-03-22 20:50:02 +0200

    Fix the request.POST, usage of formset and redirect at the end

--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import permission_required
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import ReportForm, CopyFormSet
 from .models import Report
@@ -8,16 +8,17 @@ from .models import Report
 
 @permission_required('reports.add_report', login_url='members:login')
 def add_report(request):
-    data = request.POST if request else None
+    data = request.POST if request.POST else None
     report_form = ReportForm(data)
+    formset = CopyFormSet(data, instance=request.session.get('report_in_creation', Report()))
 
     if report_form.is_valid():
-        formset = CopyFormSet(data, instance=request.session.get('report_in_creation', Report()))
         report = report_form.save()
         request.session['report_in_creation'] = formset.instance = report
         if formset.is_valid():
             formset.save()
             del request.session['report_in_creation']
+            return redirect('/members/profile/')
 
     return render(request, 'reports/add.html', locals())
 
